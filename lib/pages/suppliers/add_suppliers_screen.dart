82b009fa2fbbc68182b5c83410029cd363924337
vip7009pro <hung1893@gmail.com>
2024-10-09 17:36:59 +0700

    update supplier screen

--- lib/pages/suppliers/add_suppliers_screen.dart
+++ lib/pages/suppliers/add_suppliers_screen.dart
@@ -1,5 +1,7 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
-
+import 'package:debt_manager/controller/APIRequest.dart';
+import 'package:get/get.dart'; 
 class AddSuppliersScreen extends StatefulWidget {
   const AddSuppliersScreen({ Key? key }) : super(key: key);
 
@@ -8,6 +10,39 @@ class AddSuppliersScreen extends StatefulWidget {
 }
 
 class _AddSuppliersScreenState extends State<AddSuppliersScreen> {
+  final TextEditingController _supplierCodeController = TextEditingController();
+  final TextEditingController _supplierNameController = TextEditingController();
+  final TextEditingController _supplierAddressController = TextEditingController();
+  final TextEditingController _supplierPhoneController = TextEditingController();
+
+  Future<bool> _addSupplier(String SHOP_ID, String VENDOR_CODE, String VENDOR_NAME, String VENDOR_ADD, String VENDOR_PHONE) async {
+    // Add supplier logic here
+    bool check = true;
+    await API_Request.api_query('addvendor', {      
+      'SHOP_ID':'1',
+      'VENDOR_CODE': VENDOR_CODE, // Using supplierId as VENDOR_CODE, adjust if different
+      'VENDOR_NAME': VENDOR_NAME,
+      'VENDOR_ADD': VENDOR_ADD,
+      'VENDOR_PHONE': VENDOR_PHONE
+    }).then((value) {
+      if (value['tk_status'] == 'OK') {
+        check = true;        
+      } else {
+        check = false;
+      }
+    });
+    return check;    
+  }
+
+  @override
+  void dispose() {
+    _supplierCodeController.dispose();
+    _supplierNameController.dispose();
+    _supplierAddressController.dispose();
+    _supplierPhoneController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +55,7 @@ class _AddSuppliersScreenState extends State<AddSuppliersScreen> {
           child: ListView(
             children: [
               TextFormField(
+                controller: _supplierCodeController,
                 decoration: const InputDecoration(
                   labelText: 'Mã nhà cung cấp',
                   hintText: 'Nhập mã nhà cung cấp',
@@ -27,6 +63,7 @@ class _AddSuppliersScreenState extends State<AddSuppliersScreen> {
               ),
               const SizedBox(height: 16),
               TextFormField(
+                controller: _supplierNameController,
                 decoration: const InputDecoration(
                   labelText: 'Tên nhà cung cấp',
                   hintText: 'Nhập tên nhà cung cấp',
@@ -34,6 +71,7 @@ class _AddSuppliersScreenState extends State<AddSuppliersScreen> {
               ),
               const SizedBox(height: 16),
               TextFormField(
+                controller: _supplierAddressController,
                 decoration: const InputDecoration(
                   labelText: 'Địa chỉ nhà cung cấp',
                   hintText: 'Nhập địa chỉ nhà cung cấp',
@@ -41,6 +79,7 @@ class _AddSuppliersScreenState extends State<AddSuppliersScreen> {
               ),
               const SizedBox(height: 16),
               TextFormField(
+                controller: _supplierPhoneController,
                 decoration: const InputDecoration(
                   labelText: 'Số điện thoại nhà cung cấp',
                   hintText: 'Nhập số điện thoại nhà cung cấp',
@@ -49,8 +88,30 @@ class _AddSuppliersScreenState extends State<AddSuppliersScreen> {
               ),
               const SizedBox(height: 24),
               ElevatedButton(
-                onPressed: () {
+                onPressed: () async {
                   // Add logic to save the new supplier
+                  bool check = await _addSupplier( '1', _supplierCodeController.text, _supplierNameController.text, _supplierAddressController.text, _supplierPhoneController.text);  
+                  if (check) {
+                    AwesomeDialog(
+                      context: context,
+                      dialogType: DialogType.success,
+                      title: 'Thêm nhà cung cấp thành công',
+                      btnOkOnPress: () {
+                        Get.back(); // Close the dialog after adding  
+                      },
+                    ).show();
+                  }
+                  else {
+                    AwesomeDialog(
+                      context: context,
+                      dialogType: DialogType.error,
+                      title: 'Thêm nhà cung cấp thất bại',
+                      desc: 'Vui lòng kiểm tra lại thông tin nhà cung cấp',
+                      btnOkOnPress: () {
+                        //Get.back(); // Close the dialog after adding  
+                      },
+                    ).show();
+                  } 
                 },
                 child: const Text('Lưu nhà cung cấp'),
               ),
