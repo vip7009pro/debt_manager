c61e2fb8c8bb9a6a9bd10718a1ce33fda0893a81
Hung Nguyen <vip7009pro@gmail.com>
2024-10-19 22:28:31 +0700

    update filter warehouse input, output history

--- lib/pages/stocks/warehouse_inputHistory_screen.dart
+++ lib/pages/stocks/warehouse_inputHistory_screen.dart
@@ -3,16 +3,22 @@ import 'package:debt_manager/controller/GetXController.dart';
 import 'package:debt_manager/model/DataInterfaceClass.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
 class WarehouseInputHistoryScreen extends StatefulWidget {
   const WarehouseInputHistoryScreen({Key? key}) : super(key: key);
+
   @override
   _WarehouseInputHistoryScreenState createState() =>
       _WarehouseInputHistoryScreenState();
 }
+
 class _WarehouseInputHistoryScreenState
     extends State<WarehouseInputHistoryScreen> {
   final GlobalController c = Get.put(GlobalController());
   List<InputHistory> inputHistory = [];
+  List<InputHistory> filteredHistory = [];
+  final TextEditingController _searchController = TextEditingController();
+
   Future<List<InputHistory>> _wareHouseInputHistory() async {
     List<dynamic> inputHistory = [];
     await API_Request.api_query(
@@ -23,12 +29,28 @@ class _WarehouseInputHistoryScreenState
       return InputHistory.fromJson(item);
     }).toList();
   }
+
   void _getInputHistory() async {
     await _wareHouseInputHistory().then((value) {
       setState(() {
         inputHistory = value;
+        filteredHistory = inputHistory;
       });
     });
+  }
+
+  void _filterHistory(String query) {
+    setState(() {
+      filteredHistory = inputHistory
+          .where((element) =>
+              element.prodName!.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+@override
+  initState() {
+    super.initState();
+    _getInputHistory();
   }
   @override
   Widget build(BuildContext context) {
@@ -41,75 +63,93 @@ class _WarehouseInputHistoryScreenState
           await _wareHouseInputHistory();
           setState(() {});
         },
-        child: FutureBuilder<List<InputHistory>>(
-          future: _wareHouseInputHistory(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-              return Center(child: Text('No input history available'));
-            } else {
-              print(snapshot.data!.length); 
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  final history = snapshot.data![index];
-                  return Card(
-                    elevation: 2,
-                    margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                    child: InkWell(
-                      onTap: () {
-                        print('tapped');
-                      },
-                      child: Padding(
-                        padding: EdgeInsets.all(16),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(8),
-                              child: Image.network(
-                                'http://14.160.33.94:3010/product_images/${c.shopID.value}_${history.prodCode}_${history.prodImg?.split(',')[0] ?? ''}.jpg',
-                                width: 80,
-                                height: 80,
-                                fit: BoxFit.cover,
-                                errorBuilder: (context, error, stackTrace) {
-                                  return Icon(Icons.image_not_supported, size: 80);
-                                },
-                              ),
-                            ),
-                            SizedBox(width: 16),
-                            Expanded(
-                              child: Column(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _searchController,
+                onChanged: _filterHistory,
+                decoration: InputDecoration(
+                  labelText: 'Search',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Expanded(
+              child: FutureBuilder<List<InputHistory>>(
+                future: _wareHouseInputHistory(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Center(child: Text('No input history available'));
+                  } else {
+                    return ListView.builder(
+                      itemCount: filteredHistory.length,
+                      itemBuilder: (context, index) {
+                        final history = filteredHistory[index];
+                        return Card(
+                          elevation: 2,
+                          margin: EdgeInsets.symmetric(
+                              vertical: 8, horizontal: 16),
+                          child: InkWell(
+                            onTap: () {
+                              print('tapped');
+                            },
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Row(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text(
-                                    history.prodName ?? '',
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold,
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(8),
+                                    child: Image.network(
+                                      'http://14.160.33.94:3010/product_images/${c.shopID.value}_${history.prodCode}_${history.prodImg?.split(',')[0] ?? ''}.jpg',
+                                      width: 80,
+                                      height: 80,
+                                      fit: BoxFit.cover,
+                                      errorBuilder: (context, error, stackTrace) {
+                                        return Icon(Icons.image_not_supported, size: 80);
+                                      },
                                     ),
                                   ),
-                                  SizedBox(height: 8),
-                                  Text('Quantity: ${history.prodQty} / Date: ${history.insDate.toString().split(' ')[0]}'),
-                                  Text('Status: ${history.prodStatus} / Vendor: ${history.vendorName ?? 'N/A'}'),
-                                  Text('Bep: \$${history.bep ?? 'N/A'}'),
+                                  SizedBox(width: 16),
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          history.prodName ?? '',
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        SizedBox(height: 8),
+                                        Text('Quantity: ${history.prodQty} / Date: ${history.insDate.toString().split(' ')[0]}'),
+                                        Text('Status: ${history.prodStatus} / Vendor: ${history.vendorName ?? 'N/A'}'),
+                                        Text('Bep: \$${history.bep ?? 'N/A'}'),
+                                      ],
+                                    ),
+                                  ),
                                 ],
                               ),
                             ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
+                          ),
+                        );
+                      },
+                    );
+                  }
                 },
-              );
-            }
-          },
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
--- lib/pages/stocks/warehouse_outputHistory_screen.dart
+++ lib/pages/stocks/warehouse_outputHistory_screen.dart
@@ -3,26 +3,38 @@ import 'package:debt_manager/controller/GetXController.dart';
 import 'package:debt_manager/model/DataInterfaceClass.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
 class WarehouseOutputHistoryScreen extends StatefulWidget {
   const WarehouseOutputHistoryScreen({Key? key}) : super(key: key);
+
   @override
   _WarehouseOutputHistoryScreenState createState() =>
       _WarehouseOutputHistoryScreenState();
 }
+
 class _WarehouseOutputHistoryScreenState
     extends State<WarehouseOutputHistoryScreen> {
   final GlobalController c = Get.put(GlobalController());
   List<OutputHistory> outputHistory = [];
+  String _searchQuery = '';
+  final TextEditingController _searchController = TextEditingController();
+
   Future<List<OutputHistory>> _wareHouseOutputHistory() async {
     List<dynamic> outputHistory = [];
     await API_Request.api_query(
         'getWarehouseOutputHistory', {'SHOP_ID': c.shopID.value}).then((value) {
       outputHistory = value['data'] ?? [];
     });
-    return outputHistory.map((dynamic item) {
+
+    return outputHistory
+        .where((element) => element['PROD_NAME']
+            .toLowerCase()
+            .contains(_searchQuery.toLowerCase()))
+        .map((dynamic item) {
       return OutputHistory.fromJson(item);
     }).toList();
   }
+
   void _getOutputHistory() async {
     await _wareHouseOutputHistory().then((value) {
       setState(() {
@@ -30,6 +42,13 @@ class _WarehouseOutputHistoryScreenState
       });
     });
   }
+
+  @override 
+  void initState() {
+    super.initState();
+    _getOutputHistory();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,76 +60,100 @@ class _WarehouseOutputHistoryScreenState
           await _wareHouseOutputHistory();
           setState(() {});
         },
-        child: FutureBuilder<List<OutputHistory>>(
-          future: _wareHouseOutputHistory(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-              return Center(child: Text('No output history available'));
-            } else {
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  final history = snapshot.data![index];
-                  return Card(
-                    elevation: 2,
-                    margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                    child: InkWell(
-                      onTap: () {
-                        print('tapped');
-                      },
-                      child: Padding(
-                        padding: EdgeInsets.all(16),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(8),
-                              child: Image.network(
-                                'http://14.160.33.94:3010/product_images/${c.shopID.value}_${history.prodCode}_${history.prodImg.split(',')[0]}.jpg',
-                                width: 80,
-                                height: 80,
-                                fit: BoxFit.cover,
-                                errorBuilder: (context, error, stackTrace) {
-                                  return Icon(Icons.image_not_supported,
-                                      size: 80);
-                                },
-                              ),
-                            ),
-                            SizedBox(width: 16),
-                            Expanded(
-                              child: Column(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _searchController,
+                onChanged: (value) {
+                  setState(() {
+                    _searchQuery = value;
+                  });
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Search',
+                ),
+              ),
+            ),
+            Expanded(
+              child: FutureBuilder<List<OutputHistory>>(
+                future: _wareHouseOutputHistory(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Center(child: Text('No output history available'));
+                  } else {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        final history = snapshot.data![index];
+                        return Card(
+                          elevation: 2,
+                          margin: EdgeInsets.symmetric(
+                              vertical: 8, horizontal: 16),
+                          child: InkWell(
+                            onTap: () {
+                              print('tapped');
+                            },
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Row(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text(
-                                    history.prodName,
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold,
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(8),
+                                    child: Image.network(
+                                      'http://14.160.33.94:3010/product_images/${c.shopID.value}_${history.prodCode}_${history.prodImg.split(',')[0]}.jpg',
+                                      width: 80,
+                                      height: 80,
+                                      fit: BoxFit.cover,
+                                      errorBuilder: (context, error, stackTrace) {
+                                        return Icon(Icons.image_not_supported,
+                                            size: 80);
+                                      },
                                     ),
                                   ),
-                                  SizedBox(height: 8),
-                                  Text('Quantity: ${history.prodQty}'),
-                                  Text(
-                                      'Date: ${history.insDate.toString().split(' ')[0]}'),
-                                  Text('Customer: ${history.cusName}'),
+                                  SizedBox(width: 16),
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          history.prodName,
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        SizedBox(height: 8),
+                                        Text('Quantity: ${history.prodQty}'),
+                                        Text(
+                                            'Date: ${history.insDate.toString().split(' ')[0]}'),
+                                        Text('Customer: ${history.cusName}'),
+                                      ],
+                                    ),
+                                  ),
                                 ],
                               ),
                             ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
+                          ),
+                        );
+                      },
+                    );
+                  }
                 },
-              );
-            }
-          },
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+
